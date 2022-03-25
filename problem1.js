880154c0a86937d580eabc8710fa9313eedbc003
Sarah-Workman <55566625+Sarah-Workman@users.noreply.github.com>
2022-03-25 13:55:00 -0500

    initial commit

--- problem1.js
+++ problem1.js
@@ -1,15 +1,18 @@
 
-// call a function that will return true only when both banks have contact or when both banks have not contact 
+// call a function that will return true only when both banks have contacted or when both banks have not contacted, else return false. 
 function bankContactSyncronization(firstBank, secondBank){
     let acme = firstBank;
     let foo = secondBank;
 
+    
     if(acme & foo == true){
         return true;
     }else if(acme & foo == false){
         return true;
-    }else{acme || foo == true, false}{
+    }else{acme || foo == true}{
         return false;
     }
 
 }
+
+ 
