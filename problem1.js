c912aebb73d75d2215584219ec1c17152d5f21b2
Sarah-Workman <55566625+Sarah-Workman@users.noreply.github.com>
2022-03-31 10:09:37 -0500

    refractored code

--- problem1.js
+++ problem1.js
@@ -1,16 +1,20 @@
 
 // create a function that will return true only when both banks have contacted or when both banks have not contacted, else return false. 
 function bankContactSyncronization(firstBank, secondBank){
-      let test1 = firstBank === true && secondBank === true;
-      let test2 = firstBank === false && secondBank === false;
+      let tests = [firstBank === true && secondBank === true, firstBank === false && secondBank === false];
     
 
     
   //what I can't do...
-  //I can't have two returns without a if check 
-  //if I give the expression a true,false answer it ignores the first one. 
+  //1)I can't have two returns without a if check 
+  //2)if I give the expression a true,false it ignores the first one. 
+  //3)if I place the expression in a variables and return the variables...
+  // it returns true for the test2 and not test1. 
+  //4) If I put the expression in an array it test both expressions and returns two answers for each test. 
+  //So say both bank did not contact, it would say [true, false]. Would two answers be appropriate here?
+
   
-        return test1,test2;
+        return tests;
 
 }
 
