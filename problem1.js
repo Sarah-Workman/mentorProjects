7ddf424b317cfdbf74e5ed90677c60a99b189bff
Sarah-Workman <55566625+Sarah-Workman@users.noreply.github.com>
2022-03-31 09:54:24 -0500

    refractored code

--- problem1.js
+++ problem1.js
@@ -1,11 +1,16 @@
 
 // create a function that will return true only when both banks have contacted or when both banks have not contacted, else return false. 
 function bankContactSyncronization(firstBank, secondBank){
-   
+      let test1 = firstBank === true && secondBank === true;
+      let test2 = firstBank === false && secondBank === false;
+    
 
     
+  //what I can't do...
+  //I can't have two returns without a if check 
+  //if I give the expression a true,false answer it ignores the first one. 
   
-        return firstBank === true && secondBank === true;
+        return test1,test2;
 
 }
 
