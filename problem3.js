1633f2731160cef33c02b09b7f179af172accf20
Sarah-Workman <55566625+Sarah-Workman@users.noreply.github.com>
2022-04-15 15:53:33 -0500

    check with the try catch, still combining non numbers

--- problem3.js
+++ problem3.js
@@ -1,13 +1,7 @@
 function doubleSum(x,y){
-    try { if(x === Number && y === Number){
-        return x + y; }
-         else if(x === isNaN || y === isNaN){
-             return "Provide integers";
-         }
-        
-    } catch (error) {
-        return "Provide integers"
-        
+    if(x === isNaN || y === isNaN){
+        return "Provide Integers"
+    }else{
+        return x + y;
     }
-           
 }
