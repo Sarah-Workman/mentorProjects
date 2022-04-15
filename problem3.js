584fe4aabd50b6e90e23293a2e8da7f5e9ba8f43
Sarah-Workman <55566625+Sarah-Workman@users.noreply.github.com>
2022-04-15 16:38:11 -0500

    Number check with if conditional

--- problem3.js
+++ problem3.js
@@ -1,7 +1,7 @@
 
 // when a non number is ran by isNaN it should come back as true then return "Provide Integers", else if it comes back false it should combine the numbers. 
 function doubleSum(x,y){
-    if(x === isNaN() || y === isNaN()){
+    if( isNaN(x) || isNaN(y)){
         return "Provide Integers"
     }else{
         return x + y;
