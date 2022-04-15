e9276e48b84b08bd2fb3bc62e67c04c8db97ca71
Sarah-Workman <55566625+Sarah-Workman@users.noreply.github.com>
2022-04-15 16:31:22 -0500

    Check without the try/catch. Still having an issue combining non numbers.

--- problem3.js
+++ problem3.js
@@ -1,5 +1,7 @@
+
+// when a non number is ran by isNaN it should come back as true then return "Provide Integers", else if it comes back false it should combine the numbers. 
 function doubleSum(x,y){
-    if(x === isNaN || y === isNaN){
+    if(x === isNaN() || y === isNaN()){
         return "Provide Integers"
     }else{
         return x + y;
