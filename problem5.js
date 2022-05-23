843deb05e50445293df99bc08e369c0f3dff25d5
sarah-workman <swork623@gmail.com>
2022-05-23 15:45:03 -0500

    Repeat with a for loop

--- problem5.js
+++ problem5.js
@@ -5,6 +5,14 @@ function copyString(string,repeatAmount){
         let repeatAmount = 1;
         let result = string.repeat(repeatAmount);
         return result;
-    }else {let result = string.repeat(repeatAmount);
-    return result;}
+    }else{
+for(let i = 0; i < repeatAmount; i++){
+    return string += repeatAmount[i];
+    //return one value and one undefined value
+    //when return was placed outside of the loop I hade one
+    //string correct and rest were undefined.
+
+}
+
+}
 }
