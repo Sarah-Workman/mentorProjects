95deb1eb6bd586f9eb8882300f4dd0c26491b275
sarah-workman <swork623@gmail.com>
2022-05-26 11:06:06 -0500

    updated loop

--- problem5.js
+++ problem5.js
@@ -1,16 +1,18 @@
 function copyString(string,repeatAmount){
-
+     var output = "";
 
     if(repeatAmount === 0 || repeatAmount < 0){
-        let repeatAmount = 1;
-        let result = string.repeat(repeatAmount);
-        return result;
+      let repeatAmount = 1;
+      let result = string.repeat(repeatAmount);
+      return result;
     }else{
-        for(let i = 0; i <= repeatAmount; i++){
-        return string;
+        for(let i = 0; i < repeatAmount; i++){
+        output += string;
+        return output;
  // should return sarah every time the loop runs
 
 }
-
+   document.write("end of loop");
 }
+
 }
